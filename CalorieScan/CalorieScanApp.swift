c93ff42a81328ec05d894edfc6b8fc8d8117dcb6
Jerry Munyoka Nkongolo <jerry@kaizenflow.app>
2024-12-13 12:51:31 +0200

    Initial Commit

---
+++ CalorieScan/CalorieScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieScanApp.swift
+//  CalorieScan
+//
+//  Created by Jerry Nkongolo on 13/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct CalorieScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
