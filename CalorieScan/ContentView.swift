5e449a89fa865f08e310fab3969460b8ede8f1b6
jerrynkongolo1 <jerrynkongolo1@gmail.com>
2025-04-09 20:14:10 +0000

    fixing error

--- CalorieScan/ContentView.swift
+++ CalorieScan/ContentView.swift
@@ -5,30 +5,24 @@ struct ContentView: View {
     @StateObject private var appState = AppState()
     @StateObject private var userDataService = UserDataService.shared
     @State private var showLaunchScreen = true
-    @State private var userIsAuthenticated = false
-    @State private var isSigningInAnonymously = false
+    @State private var userIsAuthenticated = false    
 
     var body: some View {
         NavigationView {
             ZStack {
-                if isSigningInAnonymously {
-                    ProgressView("Signing in...")
-                        .onAppear {
-                            signInAnonymously()
-                        }
+                if showLaunchScreen {
+                    LaunchScreenView(onAnimationComplete: { withAnimation { showLaunchScreen = false }
+                        checkAuthentication()
+                    })
+                } else if !userIsAuthenticated {
+                    Text("Authenticating...")
+                } else if !appState.hasCompletedProfile {
+                    ProfileSetupView()
+                        .environmentObject(appState)
+                        .environmentObject(userDataService)
                 } else {
-                    if showLaunchScreen {
-                        LaunchScreenView(onAnimationComplete: { withAnimation { showLaunchScreen = false } })
-                    } else if !userIsAuthenticated {
-                        Text("Authenticating...")
-                    } else if !appState.hasCompletedProfile {
-                        ProfileSetupView()
-                            .environmentObject(appState)
-                            .environmentObject(userDataService)
-                    } else {
-                        MainTabView()
-                            .withGradientBackground()
-                    }
+                    MainTabView()
+                        .withGradientBackground()
                 }
             }
         }
@@ -39,17 +33,22 @@ struct ContentView: View {
 
     private func checkAuthentication() {
         Auth.auth().addStateDidChangeListener { auth, user in
-            userIsAuthenticated = user != nil
-            if !userIsAuthenticated {
-                isSigningInAnonymously = true
+            if let user = user {
+                userIsAuthenticated = true
+            } else {
+                userIsAuthenticated = false
+                signInAnonymously()
             }
+        if userIsAuthenticated && user != nil {
+            userDataService.refreshUserData()
         }
     }
 
     private func signInAnonymously() {
         Auth.auth().signInAnonymously { result, error in
-            isSigningInAnonymously = false
-            userIsAuthenticated = result?.user != nil
+            if let user = result?.user {
+                userIsAuthenticated = true
+            }
         }
     }
 }
